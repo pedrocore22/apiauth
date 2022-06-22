17a3d44687f2f50d9463004ac551a2294941ed5c
pedrocore22 <pedrocore22@itweblearning.com>
2022-06-22 10:19:10 +0200

    cors

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+// Abre la API a las peticiones desde Angular
+app.use(cors({origin: 'http://localhost:4200'}));
 
 const users = require('./routes/users');
 app.use(express.json()); // Parsea los JSON entrantes a JavaScript;
